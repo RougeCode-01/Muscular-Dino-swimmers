1fdfcf6784135eae7690cdb9a0b44966383c70e5
Christian Rojas <koenigrojass@gmail.com>
2024-07-24 10:48:58 -0600

    Started the seaweed
    It activated the lightning "effect" when the player gets close and if it hits it it'll take damage

---
+++ Muscular-Dino-swimmers/Assets/Scripts/Lightning.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+public class Lightning : MonoBehaviour
+{
+    [SerializeField] private GameObject LightningSprite;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DamagePlayer();
+        }
+    }
+    
+    private void DamagePlayer()
+    {
+        //Damage player using health manager
+        Debug.Log("Youve been hit by lightning");
+    }
+    
+    //once this game object is activated, it will change the alpha of the sprite to make it flicker
+    private void FlikkerLightning()
+    {
+        //change the alpha of the sprite to make it flicker
+        Debug.Log("Lightning is flickering");
+    }
+    
+}
