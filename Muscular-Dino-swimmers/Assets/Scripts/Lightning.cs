47f908b9fe8d440a6ec47b7fe10f42121f24fade
Christian Rojas <koenigrojass@gmail.com>
2024-07-24 11:22:38 -0600

    Seaweed is done Ill make it look better afterwards

--- Muscular-Dino-swimmers/Assets/Scripts/Lightning.cs
+++ Muscular-Dino-swimmers/Assets/Scripts/Lightning.cs
@@ -1,28 +1,85 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
+
 public class Lightning : MonoBehaviour
 {
-    [SerializeField] private GameObject LightningSprite;
+    [SerializeField] private float flickerDuration = 0.1f; // Duration of each flicker
+    [SerializeField] private float flickerInterval = 0.2f; // Interval between flickers
+
+    private Coroutine flickerCoroutine;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("SpriteRenderer component not found on the GameObject");
+        }
+    }
+
+    private void OnEnable()
+    {
+        StartFlicker();
+    }
+
+    private void OnDisable()
+    {
+        StopFlicker();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            // Deal damage to the player
             DamagePlayer();
         }
     }
     
     private void DamagePlayer()
     {
-        //Damage player using health manager
-        Debug.Log("Youve been hit by lightning");
+        // Deal damage to the player
+        Debug.Log("Player has been damaged by the lightning!");
     }
-    
-    //once this game object is activated, it will change the alpha of the sprite to make it flicker
-    private void FlikkerLightning()
+
+    private void StartFlicker()
     {
-        //change the alpha of the sprite to make it flicker
-        Debug.Log("Lightning is flickering");
+        if (flickerCoroutine == null)
+        {
+            flickerCoroutine = StartCoroutine(FlickerLightning());
+        }
     }
-    
+
+    private void StopFlicker()
+    {
+        if (flickerCoroutine != null)
+        {
+            StopCoroutine(flickerCoroutine);
+            flickerCoroutine = null;
+            SetAlpha(1f); // Reset alpha to fully visible
+        }
+    }
+
+    private IEnumerator FlickerLightning()
+    {
+        while (true)
+        {
+            SetAlpha(0.5f); // Make the sprite invisible
+            yield return new WaitForSeconds(flickerDuration);
+            SetAlpha(1f); // Make the sprite visible
+            yield return new WaitForSeconds(flickerInterval);
+        }
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        if (spriteRenderer != null)
+        {
+            Color color = spriteRenderer.color;
+            color.a = alpha;
+            spriteRenderer.color = color;
+        }
+    }
 }
