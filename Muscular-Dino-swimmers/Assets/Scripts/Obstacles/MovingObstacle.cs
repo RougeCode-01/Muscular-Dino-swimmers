325b96d7463aec573c83174a3e243708d8db35ba
safdfadf <maahuja123@gmail.com>
2024-08-01 13:14:09 -0600

    Moving_Obstacle added
    Prefab

---
+++ Muscular-Dino-swimmers/Assets/Scripts/Obstacles/MovingObstacle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 3.0f; // Speed of the obstacle
+    public float moveDistance = 5.0f; // Distance the obstacle moves back and forth
+
+    private Vector3 startPosition;
+    private bool movingRight = true;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Calculate movement
+        float movement = speed * Time.deltaTime;
+        if (movingRight)
+        {
+            transform.Translate(movement, 0, 0);
+            if (transform.position.x >= startPosition.x + moveDistance)
+                movingRight = false;
+        }
+        else
+        {
+            transform.Translate(-movement, 0, 0);
+            if (transform.position.x <= startPosition.x - moveDistance)
+                movingRight = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject); // Destroy the player on collision
+        }
+    }
+}
